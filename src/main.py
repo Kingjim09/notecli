6b8e14967fa79aa2efb799802fe62e12aa3c97c5
Aeron Asug <an0nymnx@outlook.com>
2025-04-30 23:27:18 -0600

    refactor:(main) update user input function to prompt_user_for_input with available_choices argument

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import sys
 import time
 from typing import Any, List, Dict
 from pathlib import Path
-from utils.communication import communicate_with_user
+from utils.communication import prompt_user_for_input
 from utils.file_utils import (
   check_if_file_or_dir,
   change_path,
@@ -33,7 +33,7 @@ current_path: str = get_os()
 def main() -> None:
   """Handles the main logic for user interaction and action selection."""
   global entries, current_path
-  user_choice: Dict[str, Any] = communicate_with_user("list", "option", "What would you like to do?", choices=choices)
+  user_choice: Dict[str, Any] = prompt_user_for_input("list", "option", "What would you like to do?", available_choices=choices)
   
   try:
     if user_choice:
@@ -47,14 +47,14 @@ def main() -> None:
           print(f"User has selected '{selected_option}'.\n")
         case "Create File":
           while True:
-            new_file_name_input: Dict[str, str] = communicate_with_user("input", "new file", "What will be the name of the new file?")
+            new_file_name_input: Dict[str, str] = prompt_user_for_input("input", "new file", "What will be the name of the new file?")
             current_path = Path(current_path)
             new_file_path: Path = current_path / new_file_name_input["new file"]
 
             if new_file_path.name.strip() != "":
-              should_include_content: Dict[str, bool] = communicate_with_user("confirm", "include file content", "Do you want to add content to the file?", True)
+              should_include_content: Dict[str, bool] = prompt_user_for_input("confirm", "include file content", "Do you want to add content to the file?", True)
               if should_include_content["include file content"]:
-                new_file_content: Dict[str, str] = communicate_with_user("input", "file content", f"What would be the file content for {new_file_path}?")
+                new_file_content: Dict[str, str] = prompt_user_for_input("input", "file content", f"What would be the file content for {new_file_path}?")
                 create_file(new_file_path, new_file_content["file content"])
               else:
                 create_file(new_file_path)
@@ -65,7 +65,7 @@ def main() -> None:
               text_animation("Please enter a valid file name.")
         case "Create Folder":
           while True:
-            new_folder_name_input: Dict[str, str] = communicate_with_user("input", "folder name", "What will be the name of the new folder?")
+            new_folder_name_input: Dict[str, str] = prompt_user_for_input("input", "folder name", "What will be the name of the new folder?")
             new_folder_name: str = new_folder_name_input["folder name"].strip()
             if new_folder_name != "":
               current_path = Path(current_path)
@@ -80,7 +80,7 @@ def main() -> None:
           entries = change_path(new_path)
         case "Delete":
           clear_terminal()
-          delete_choice: Dict[str, str] = communicate_with_user("list", "option", "What would you like to delete?", choices=[*entries, "Exit"])
+          delete_choice: Dict[str, str] = prompt_user_for_input("list", "option", "What would you like to delete?", available_choices=[*entries, "Exit"])
           if delete_choice["option"] == "Exit":
             text_animation(message="Going back...")
             print()
@@ -90,7 +90,7 @@ def main() -> None:
         case "Help":
           clear_terminal()
           show_help()
-          communicate_with_user()
+          prompt_user_for_input()
           clear_terminal()
         case "Exit":
           text_animation(message="Thank you for using the Note CLI. Goodbye!...") 
@@ -106,7 +106,7 @@ def main() -> None:
               print(f"'{selected_option}' is a Directory.\n")
             case None:
               print(f"'{selected_option}' is neither a File or Directory.\n")
-          communicate_with_user()
+          prompt_user_for_input()
     else:
       raise Exception("Something happened when selecting your choice.")
   except Exception as e:
